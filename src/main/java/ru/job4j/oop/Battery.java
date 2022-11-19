e07b71b5e00a0c7cfc552a481cde1242bbfdc4ef
decrent <mortalkombinat@gmail.com>
2022-11-19 19:26:18 +0700

    1.8. Состояние объекта [#167388]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -4,14 +4,13 @@ public class Battery {
 
     private int load;
 
-    public Battery(int load){
+    public Battery(int load) {
         this.load = load;
     }
 
     public int getLoad() {
         return load;
     }
-
 
     public void exchange(Battery another) {
         this.load = this.load + another.load;
